0ccfa807ffb4ba7ff7b905ca03c77fa0451557df
Andre Espinoza <ae95caba@gmail.com>
2023-06-10 01:18:12 -0300

    display all the messages on the homepage. Members will see the full message info(timestamp and author)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const Message = require("../models/message");
+const asyncHandler = require("express-async-handler");
 
 // Require controller modules.
 const user_controller = require("../controllers/userController");
@@ -14,9 +16,16 @@ user logged in (by checking the cookies that come in
 		 user to the request object for us. So, all we need to
 			 do is check for req.user to change our view depending
 				 on whether or not a user is logged in. */
-router.get("/", function (req, res, next) {
-  res.render("index", { user: req.user });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const messages = await Message.find().populate("user").exec();
+
+    console.log(`the user is ${req.user}`);
+
+    res.render("index", { user: req.user, messages: messages });
+  })
+);
 
 router.get("/sign-up", signup_controller.signup_create_get);
 
